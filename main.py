1b66c2a1fc434cd396b0c2d92ec30b40f52c9d64
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2016-09-22 23:09:16 -0300

    Item sets guaranteed to be completely filled

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import os
 import json
 import csv
+import random
 
 __author__ = "rpalmaotero"
 
 
 N_POSITIONS = 12750
-N_FREQ_ITEMS_SET = 60
-MAX_FREQ_ITEMS_SET_LENGTH = N_POSITIONS // N_FREQ_ITEMS_SET
+N_LARGE_ITEMS_SET = 30
+N_SMALL_ITEMS_SET = 30
+N_ITEMS_SET = N_LARGE_ITEMS_SET + N_SMALL_ITEMS_SET
+MAX_LARGE_ITEMS_SET_LENGTH = 225
+MAX_SMALL_ITEMS_SET_LENGTH = 200
 DB_PATH = os.path.join(os.path.dirname(__file__), "./cleaned_db.dat")
 OUTPUT_FILENAME = "families"
 
+
+class Family(list):
+    def __init__(self, max_length):
+        super().__init__()
+
+        # privado?
+        self.MAX_LENGTH = max_length
+
+    @property
+    def full(self):
+        return self.MAX_LENGTH - 1 <= len(self)
 
 class FrequentItemsSets:
     def __init__(self):
@@ -54,43 +69,87 @@ class FrequentItemsSets:
                 ),
                 key=lambda record: record[1],
                 reverse=True
-            )[:N_FREQ_ITEMS_SET]
+            )[:N_ITEMS_SET]
         ))
 
-        for top_product in top_products:
-            self.families[top_product] = []
+        for i, top_product in enumerate(top_products):
+            if i >= 30:
+                family = Family(MAX_LARGE_ITEMS_SET_LENGTH)
+            else:
+                family = Family(MAX_SMALL_ITEMS_SET_LENGTH)
+            self.families[top_product] = family
             del self.affinities[top_product]
 
     #TODO: fijarnos en sku a sku? fijarnos directamente en afinidad de tops?
     def assign_products_to_families(self):
-        left_alone = 0
+        left_alone = []
         for sku, record in self.affinities.items():
-            top_affinities = sorted(
-                filter(
-                    lambda item: item[0] in self.families,
-                    record["affinities"].items()
-                ),
+            sorted_affinities = sorted(
+                record["affinities"].items(),
                 key=lambda item: item[1],
                 reverse=True
             )
-
+            top_affinities = filter(
+                    lambda item: item[0] in self.families,
+                    sorted_affinities
+            )
             inserted = False
             for top_sku, _ in top_affinities:
-                if len(self.families[top_sku]) < MAX_FREQ_ITEMS_SET_LENGTH - 1:
+                if not self.families[top_sku].full:
                     self.families[top_sku].append(sku)
                     inserted = True
                     break
 
             if not inserted:
-                left_alone += 1
-                print("WARNING: {} left alone...".format(sku))
+                # Si no entra a ninguna familia...
+                # DRY!!!
+                other_affinities = filter(
+                    lambda item: item[0] not in self.families,
+                    sorted_affinities
+                )
+                for other_sku, _ in other_affinities:
+                    for family_sku, family in self.families.items():
+                        if other_sku in family and not family.full:
+                            family.append(sku)
+                            inserted = True
+                            break
+                    if inserted:
+                        break
 
-        print("WARNING: {} SKUs left alone...".format(left_alone))
+            if not inserted:
+                left_alone.append(sku)
+                print("WARNING: {} left alone...".format(sku))
+                print(record)
+                print("\n", end="")
+
+
+        print("INFO: {} SKUs left alone...".format(left_alone))
+
+        random.shuffle(left_alone)
+        for left_alone_sku in left_alone:
+            for _, family in self.families.items():
+                if not family.full:
+                    family.append(left_alone_sku)
+                    break
+
+        print("INFO: So far...")
+
+        for family_sku, family in self.families.items():
+            print("INFO: {} family ({} capacity) has {} SKUs...".format(
+                family_sku,
+                family.MAX_LENGTH,
+                len(family) + 1
+            ))
+
+
 
     def output_families(self):
         families = list(map(
             lambda family: [family[0]] + family[1],
-            self.families.items()
+            sorted(
+                self.families.items(),
+                key=lambda item: len(item[1])
+            )
         ))
 
         with open("{}.json".format(OUTPUT_FILENAME), "w") as output_file:
